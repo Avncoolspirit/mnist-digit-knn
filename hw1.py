c5f0cfc3b232ed2af591640ef273063e4bb27c92
Yogesh Verma <yogeshverma@Yogeshs-MacBook-Pro.local>
2019-09-13 22:24:06 -0400

    digit display, prior probability

---
+++ hw1.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep 13 22:21:16 2019
+
+@author: yogeshverma
+"""
+
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+df = pd.read_csv('/Users/yogeshverma/Documents/Fall 19/AML/Homework 1/digit-recognizer/train.csv')
+
+number_pixel_dict = {}
+pixels = df.iloc[ : , 1:]
+labels = df.iloc[ : , 0]
+dataset = df.values
+
+#
+for index_label in range(0,len(dataset)):
+    if dataset[index_label][0] not in number_pixel_dict.keys():
+        number_pixel_dict[dataset[index_label][0]]= {'count':1,'number_array':dataset[index_label][1:]}
+    else:
+        number_pixel_dict[dataset[index_label][0]]['count']+=1
+        
+        
+total_prob=0
+for label in range(0,10):
+    pixel_array = np.array(number_pixel_dict[label]['number_array'], dtype = 'uint8').reshape((28,28))
+    prior_prob = (number_pixel_dict[label]['count'])/42000.0
+    number_pixel_dict[label]['prior_prob']=prior_prob
+    total_prob+=prior_prob
+    print (prior_prob,total_prob)
+    #plt.title('Label is {Label}'.format(Label = labels))
+    plt.imshow(pixel_array, cmap = 'gray')
+    plt.show()
