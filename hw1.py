f45ef3c96cd2a47b1b7c3ba2485ab10297126eec
System Administrator <root@Avnishs-MacBook-Air-2.local>
2019-09-18 19:33:58 -0400

    completed the square distance

--- hw1.py
+++ hw1.py
@@ -11,7 +11,7 @@ dataset = df.values
 
 for index_label in range(0,len(dataset)):
     if dataset[index_label][0] not in number_pixel_dict.keys():
-        number_pixel_dict[dataset[index_label][0]]= {'count':1,'number_array':dataset[index_label][1:]}
+        number_pixel_dict[dataset[index_label][0]]= {'count':1,'number_array':dataset[index_label][1:],'index':index_label}
     else:
         number_pixel_dict[dataset[index_label][0]]['count']+=1
         
@@ -21,13 +21,20 @@ for label in range(0,10):
     pixel_array = np.array(number_pixel_dict[label]['number_array'], dtype = 'uint8').reshape((28,28))
     prior_prob = (number_pixel_dict[label]['count'])/42000.0
     number_pixel_dict[label]['prior_prob']=prior_prob  
-    plt.title('Label is {Label}'.format(Label = labels))
-    plt.imshow(pixel_array, cmap = 'gray')
-    plt.show()
+#    plt.title('Label is {Label}'.format(Label = labels))
+#    plt.imshow(pixel_array, cmap = 'gray')
+#    plt.show()
 
 for label in range(0,10):
     print("Prior Probability for" , label, "is ", number_pixel_dict[label]['prior_prob'])
     
-print(number_pixel_dict)
+tile_matrix = np.tile(number_pixel_dict[1]['number_array'],(42000,1))
+dataset_numpy = pixels.to_numpy()
+subtraction_matrice = np.subtract(tile_matrix,dataset_numpy)
+ones=np.ones(784)
+square_row=np.square(subtraction_matrice) 
+distance_squared =  square_row*ones
+distance = sorted(np.sum(distance_squared,axis=1))
 
+    
 
